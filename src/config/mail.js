422113222f0e78702612671f5eae4cf4d690e20f
Tierry <tierry.ray@gmail.com>
2019-07-11 13:52:31 -0300

    Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '99552df0148be5',
+    pass: 'e8a8c1f9205aea',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
