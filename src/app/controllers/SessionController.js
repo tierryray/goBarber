2d2fc6d6c56e91287b657afa5153489c40529bd2
Tierry Ray <tierry.machado@agilecontent.com>
2020-01-13 22:44:53 -0300

    ♻️ Adicionando Cors, mudando retorno das chamadas

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 import authConfig from '../../config/auth';
 
@@ -19,7 +20,17 @@ class SessionController {
     }
 
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -29,13 +40,15 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, nome } = user;
+    const { id, nome, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         nome,
         email,
+        provider,
+        avatar,
       },
       // ID para ser aproveitado no token e Auth Properties
       token: jwt.sign({ id }, authConfig.secret, {
