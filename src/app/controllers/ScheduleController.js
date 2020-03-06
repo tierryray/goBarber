5e51bb8a8c6519badafe27a133235932b0798290
Tierry <tierry.ray@gmail.com>
2020-03-06 17:53:24 -0300

    fix bug!

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -29,7 +29,7 @@ class ScheduleController {
         {
           model: User,
           as: 'user',
-          attributes: ['name'],
+          attributes: ['nome'],
         },
       ],
       order: ['date'],
